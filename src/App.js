28116772a6fa8cf9c321f12d2ef780240586acda
Iftekhar Alvee <alveee1998@gmail.com>
2021-02-26 17:44:27 +0600

    fff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import reactDom from 'react-dom';
 
 function App() {
+  let style= {
+    color: 'red',
+    backgroundColor: 'white'
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <Person name="Rubel" naika="Moushumi" ></Person>
+        <Person name="Tubel" naika="Toumi"></Person>
+        <Person name="Bappa" naika="Tappa"></Person>
+        <Person name="Tommy" naika="Nini"></Person>
+        <Person name="Vin" naika="Yummy"></Person>
+        <Person name="Bangla" naika="Hind"></Person>
       </header>
     </div>
   );
+}
+
+
+function Person(props) {
+  return (
+    <div style={{border: '2px solid red', margin: '10px', padding:'10px'}}>
+      <h1>Nayok: {props.name}</h1>
+      <h3>Naika: {props.naika}</h3>
+    </div>
+  )
 }
 
 export default App;
